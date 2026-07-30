bd058593c02c5f63b71b1264b67dd8a0bb870078
Arun <allamarunkumar@gmail.com>
2026-07-30 17:04:34 +0530

    test

--- OopsConsoleProject/Class4.cs
+++ OopsConsoleProject/Class4.cs
@@ -14,14 +14,11 @@ namespace OopsConsoleProject
             s1.StudentId = 111;
             s1.StudentName = "Test";
             s1.Marks = 450;
-
             Console.WriteLine($"Student ID      :{s1.StudentId}");
             Console.WriteLine($"Student Name    :{s1.StudentName}");
             Console.WriteLine($"Student Marks   :{s1.Marks}");
             Console.WriteLine($"College Name    :{Student.CollegeName}");
-            Console.WriteLine($"College Address :{Student.Address}");
-            
-            
+            Console.WriteLine($"College Address :{Student.Address}");            
         }
     }
 }

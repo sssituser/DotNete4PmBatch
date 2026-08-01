a3edb4f96afed44757c4a7b7c889d3016ed54376
Arun <allamarunkumar@gmail.com>
2026-08-01 17:52:00 +0530

    tesss

---
+++ OopsConsoleProject/Class8.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConsoleProject
+{
+    class Area
+    {
+        private int length;
+
+        private int breadh;
+
+        private int side;
+        private double radius;
+
+    
+
+        public int AreaOfRectangle
+        {
+            get { return length*breadh; }
+            
+        }
+        public int AreaOfSquare
+        {
+            get
+            {
+                return side * side;
+            }
+            
+         }
+
+        public double AreaOfCirle
+        {
+            get
+            {
+                return 3.14 * radius * radius;
+            }
+        }
+        public double Radius
+        {
+           
+            set { radius = value; }
+        }
+
+        public int Side
+        {
+           
+            set { side = value; }
+        }
+
+        public int Breadh
+        {
+           
+            set { breadh = value; }
+        }
+
+        public int Length
+        {
+            
+            set { length = value; }
+        }
+
+    }
+    internal class Class8
+    {
+        static void Main(string[] args)
+        {
+            Area a = new Area();
+            a.Length = 5;
+            a.Breadh = 4;
+            a.Side = 6;
+            a.Radius = 5.6;
+
+
+            Console.WriteLine(a.AreaOfRectangle);
+            Console.WriteLine(a.AreaOfSquare);
+            Console.WriteLine(a.AreaOfCirle);
+        }
+    }
+}

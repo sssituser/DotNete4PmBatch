7c5741849027b3feecad6208505df1deb57b3151
arunkumar <allamarunkumar@gmail.com>
2026-08-26 17:53:10 +0530

    Test

---
+++ FileHandling/Class8.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace FileHandling
+{
+    internal class Class8
+    {
+        static void Main(string[] args)
+        {
+
+
+          
+            foreach(DriveInfo drInfo in DriveInfo.GetDrives())
+            {
+                long totalSpaceSize = drInfo.TotalSize / (1024*1024*1024);
+                long freeSpaceSize = drInfo.TotalFreeSpace / (1024*1024*1024);
+                long useSpaceSize = totalSpaceSize - freeSpaceSize;
+                Console.WriteLine($"========================================================================================================");
+                Console.WriteLine($"{drInfo.Name.Substring(0,drInfo.Name.Length-2)} Drive\t\tTotal Space : {totalSpaceSize} GB\t\tUsed Space : {useSpaceSize} GB\t\tFree Space : {freeSpaceSize} GB");
+                Console.WriteLine($"===========================Folders Present In the {drInfo.Name.Substring(0,drInfo.Name.Length-2)} Drive================================================");
+                DirectoryInfo info = new DirectoryInfo(drInfo.Name);
+                foreach(DirectoryInfo dinfo in info.GetDirectories())
+                {
+                    Console.WriteLine(dinfo.Name);
+                }
+              
+
+                Console.WriteLine($"===========================Files Present In the {drInfo.Name.Substring(0, drInfo.Name.Length - 2)} Drive================================================");
+             
+                foreach (FileInfo finfo in info.GetFiles())
+                {
+                    Console.WriteLine(finfo.Name);
+                }
+                Console.WriteLine($"========================================================================================================");
+
+
+            }
+        }
+    }
+}
---
+++ FileHandling/Class9.cs
@@ -0,0 +1,23 @@
+﻿using MySystem;
+using System;
+
+
+namespace FileHandling   // Application
+{
+    internal class Class9
+    {
+        static void Main(string[] args)
+        {
+            MyConsole.MyWriteLine("=========This is Example For Library================");
+            MyConsole.MyWrite("Enter an Integer : ");
+            int num = MyConsole.ReadInt();
+            MyConsole.MyWrite("Enter a Double Value : ");
+            double dnum = MyConsole.ReadDouble();
+
+            MyConsole.MyWriteLine($"{new MyNums().IsPalindrome(num)}");
+            MyConsole.MyWriteLine($"{new MyNums().Square(num)}");
+            MyConsole.MyWriteLine($"{new MyNums().Reverse(num)}");
+            MyConsole.MyWriteLine($"{new MyNums().IsAdam(num)}");
+        }
+    }
+}
